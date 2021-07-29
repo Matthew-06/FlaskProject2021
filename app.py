46a9f057743bdfe1bf6ea358b0d6d84e811b1370
Matthew <mattvarela06@gmail.com>
2021-07-29 14:05:39 +0000

    inital commit

--- app.py
+++ app.py
@@ -1,16 +1,72 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
+
 
 
 # -- Initialization section --
 app = Flask(__name__)
 
-
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template("index.html")
+  
+  
+
+@app.route('/gethelp')
+def gethelp():
+    return render_template("gethelp.html")
+
+
+
+
+@app.route('/log-in')
+def login():
+    return render_template("log-in.html")
+
+
+@app.route('/sign-up')
+def signup():
+    return render_template("sign-up.html")
+
+
+@app.route('/signed-in', methods= ["POST", "GET"])
+def signedin():
+    print(request.form["uname"])
+    user_name = request.form["uname"]  
+    if request.method == "POST": 
+        return render_template("signed-in.html", user_name= user_name)
+    elif request.method == "get":
+        return "404 Error"
+  
+
+@app.route('/forgotpassword')
+def forgorpws():
+    return render_template("forgot-password.html")
+
+
+@app.route('/chatroom')
+def chatroom():
+    return render_template("chatroom.html")
+
+
+@app.route('/talk')
+def talk():
+    return render_template("talk.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
